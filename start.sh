4fdde00dac3e5e0a883b22e96ac55b72d92ad09e
smentasti <simone.mentasti@polimi.it>
2024-07-24 11:36:22 +0200

    first commit

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Specify the container name
+CONTAINER_NAME="drims2"
+
+# Check if the container exists
+if docker ps -a | grep -q $CONTAINER_NAME; then
+    echo "Container $CONTAINER_NAME exists."
+
+    # Check if the container is running
+    if [ "$(docker inspect -f {{.State.Running}} $CONTAINER_NAME)" == "true" ]; then
+        echo "Container $CONTAINER_NAME is running. Stopping it now..."
+        docker stop $CONTAINER_NAME
+        docker rm $CONTAINER_NAME
+    else
+        echo "Container $CONTAINER_NAME is not running."
+        docker rm $CONTAINER_NAME
+    fi
+else
+    echo "Container $CONTAINER_NAME does not exist."
+fi
+
+docker run -it --privileged -v /dev:/dev --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --net=host --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$(pwd)/data:/data"  --name drims2 smentasti:drims2  
