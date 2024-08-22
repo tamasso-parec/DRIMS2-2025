567099b4d9516251d6898d9ada96dd08d9e03d3f
smentasti <simone.mentasti@polimi.it>
2024-08-22 07:34:57 +0200

    added script to connect to running container

---
+++ connect.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Variables
+CONTAINER_NAME="drims2"
+
+# Check if the container is running
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Container $CONTAINER_NAME is running. Connecting to it..."
+    docker exec -it $CONTAINER_NAME /bin/bash
+else
+    echo "Container $CONTAINER_NAME is not running."
+fi
