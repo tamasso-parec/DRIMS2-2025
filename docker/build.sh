30ce7c90f41034774a0aed3358cf3cc17b3f6c54
Simone Mentasti <simone.mentasti@polimi.it>
2025-06-25 16:20:47 +0200

    updated Dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker buildx build --platform linux/amd64,linux/arm64  -t smentasti/drims2 --push . 
+docker buildx build --platform linux/amd64,linux/arm64  -t smentasti/drims2:2025 --push . 
 
